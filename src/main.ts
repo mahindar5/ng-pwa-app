db97323eb764aa6fcef50776bf82fa0bad45af7d
mahindar5 <mahindar_527@yahoo.com>
2025-04-04 18:41:31 -0700

    Refactor workflow to publish packages and update versioning process

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { fetchProxy } from '@mahindar5/common-lib';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
+import { BUILD_INFO } from './build-info';
 
 fetchProxy();
 bootstrapApplication(AppComponent, appConfig)
 	.catch((err) => console.error(err));
+(window as any).globalConfig = BUILD_INFO;
