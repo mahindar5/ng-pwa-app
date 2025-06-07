acfde584eac676aeba5263929a9463063d549c7c
mahindar5 <mahindar_527@yahoo.com>
2025-06-07 01:05:38 -0700

    refactor: simplify BUILD_INFO imports and configuration in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { BUILD_INFO as commonLibBUILD_INFO, fetchProxy } from '@mahindar5/common-lib';
-import { configureApplicationWithConfig, BUILD_INFO as matBUILD_INFO } from '@mahindar5/ng-common-ion-lib';
+import { fetchProxy } from '@mahindar5/common-lib';
+import { configureApplicationWithConfig } from '@mahindar5/ng-common-ion-lib';
 import { AppComponent } from '@mahindar5/ng-common-ion-lib/components/app';
-import { BUILD_INFO as myAppBUILD_INFO } from './build-info';
+import { BUILD_INFO } from './build-info';
 
 fetchProxy();
-bootstrapApplication(AppComponent, await configureApplicationWithConfig({ BUILD_INFO: { myAppBUILD_INFO, matBUILD_INFO, commonLibBUILD_INFO } }))
+bootstrapApplication(AppComponent, await configureApplicationWithConfig({ BUILD_INFO: { myAppBUILD_INFO: BUILD_INFO } }))
 	.catch((err) => console.error(err));
